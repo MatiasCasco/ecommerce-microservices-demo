032ff1a8eb1be6117b5cfeaa76a06fabce1def91
MatiasCasco <matiascasco_96@hotmail.com>
2026-03-14 13:56:09 -0300

    Commit se agrego 1 modelo en cada componente

---
+++ product-service/src/main/java/com/ecommerce/productservice/model/Product.java
@@ -0,0 +1,11 @@
+package com.ecommerce.productservice.model;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class ProductDTO {
+    private Long id;
+    private String name;
+}
