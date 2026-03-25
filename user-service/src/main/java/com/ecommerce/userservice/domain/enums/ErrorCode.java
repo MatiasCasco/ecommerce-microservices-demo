465d9c7673e3424537927e4ead10667b61f13f50
MatiasCasco <matiascasco_96@hotmail.com>
2026-03-24 21:02:27 -0300

    Se agrego datos en enum y se separo mentalmente

--- user-service/src/main/java/com/ecommerce/userservice/domain/enums/ErrorCode.java
+++ user-service/src/main/java/com/ecommerce/userservice/domain/enums/ErrorCode.java
@@ -2,8 +2,15 @@ package com.ecommerce.userservice.domain.enums;
 
 public enum ErrorCode {
 
+    // 🔵 NEGOCIO
     USER_NOT_FOUND(404, "USER_NOT_FOUND"),
+    EMAIL_ALREADY_EXISTS(400, "EMAIL_ALREADY_EXISTS"),
+    USER_DISABLED(403, "USER_DISABLED"),
+
+    // 🟡 VALIDACIÓN
     INVALID_INPUT(400, "INVALID_INPUT"),
+
+    // 🔴 SISTEMA
     INTERNAL_ERROR(500, "INTERNAL_ERROR");
 
     private final int status;
