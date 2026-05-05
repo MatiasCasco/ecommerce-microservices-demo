3a1f13996c28fafe7356ebb5e42a7620681f64d3
MatiasCasco <matiascasco_96@hotmail.com>
2026-05-05 13:26:22 -0300

    feat(product-service): add pagination, sorting and dynamic filtering with specification
    - implement dynamic filtering using JPA Specification
    - add pagination support with Pageable
    - enable sorting via query params
    - add default pagination and sorting configuration
    - update controller to return Page<ProductResponse

--- product-service/src/main/java/com/ecommerce/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -9,6 +9,10 @@ import com.ecommerce.productservice.service.ProductService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -29,8 +33,16 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<ProductResponse> findAll(ProductFilter filter) {
-        return productService.getAllProducts(filter);
+    public Page<ProductResponse> findAll(
+            @ModelAttribute  ProductFilter filter,
+            @PageableDefault(
+                    page = 0,
+                    size = 100,
+                    sort = "id",
+                    direction = Sort.Direction.ASC
+            ) Pageable pageable
+            ) {
+        return productService.getAllProducts(filter,  pageable);
     }
 
     @GetMapping("/{id}")
--- product-service/src/main/java/com/ecommerce/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/ecommerce/productservice/repository/ProductRepository.java
@@ -3,8 +3,10 @@ package com.ecommerce.productservice.repository;
 import com.ecommerce.productservice.domain.entity.Product;
 import com.ecommerce.productservice.domain.enums.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
     List<Product> findByStatus(ProductStatus status);
 }
